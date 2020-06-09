4b2fbb7c4c46ab590c9366c7f59683a2aa0595c5
lyb <1665488919@qq.com>
2020-06-09 18:54:48 +0800

    啊啊啊谢谢谢谢

---
+++ idea-git-test/src/main/java/b.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class b {
+    @Test
+    public void test(){
+        String name="易都伟";
+        String value="吃shi";
+        System.out.println(name+value);
+    }
+}
